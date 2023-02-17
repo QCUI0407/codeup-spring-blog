a8b630532fc8252549923b4cfbf2c536dd13b6b2
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2023-02-17 09:39:15 -0600

    Controller exe

---
+++ src/main/java/com/codeup/codeupspringblog/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/{num}/and/{num2}")
+    @ResponseBody
+    public String addNum(@PathVariable int num, @PathVariable int num2){
+//        return String.valueOf(num + num2);
+        return num + "+" + num2 + "=" + (num+num2);
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public int subtractNum(@PathVariable int num1, @PathVariable int num2){
+        return num2 - num1;
+    }
+
+    @GetMapping("/multiply/{num1}/from/{num2}")
+    @ResponseBody
+    public int multiplyNum(@PathVariable int num1, @PathVariable int num2){
+        return num2 * num1;
+    }
+
+    @GetMapping("/divide/{num1}/from/{num2}")
+    @ResponseBody
+    public double divideNum(@PathVariable double num1, @PathVariable double num2){
+        return num1 / num2;
+    }
+}
